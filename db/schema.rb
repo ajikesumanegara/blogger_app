a0f3a849a8b5c8ef1d9898bc5944d97154a406e5
ajikesumanegara <ajikesuma25@gmail.com>
2024-05-06 16:37:00 +0700

    install BCrypt, add password digest column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_03_072926) do
+ActiveRecord::Schema.define(version: 2024_05_06_090753) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2024_05_03_072926) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
